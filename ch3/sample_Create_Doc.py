cdf4cc3d785f2f786bcb655f88a78fa6b2eb1fa0
Saiqul Haq <saiqulhaq@gmail.com>
2024-06-15 23:56:41 +0700

    WIP chapter 3

--- ch3/sample_Create_Doc.py
+++
@@ -1,9 +0,0 @@
-from llama_index.core import Document
-
-text = "The quick brown fox jumps over the lazy dog."
-doc = Document(
-    text=text, 
-    metadata={'author': 'John Doe','category': 'others'}, 
-    id_='1'
-)
-print(doc)
