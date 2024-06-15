8f48bdbb1f548ca3a03d09d95d795e1bbe491a2f
Saiqul Haq <saiqulhaq@gmail.com>
2024-06-15 20:35:38 +0700

    WIP chapter 3

--- ch2/sample1.py
+++
@@ -1,8 +0,0 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-
-documents = SimpleDirectoryReader('files').load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query("summarize each document in a few sentences")
-
-print(response)
